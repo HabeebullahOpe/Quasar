5bcdd321b3ca8b4e82d54c044d93b2ccc7a31827
Beeb <opeyemihabeebullah279@gmail.com>
2025-10-17 17:30:41 +0100

    Used embedded and not just direct link

--- memmer.py
+++ memmer.py
@@ -13,29 +13,27 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Your meme database
 meme_database = {
     "slimer": [
-        "https://tenor.com/bRMz0.gif"
+        "https://media1.tenor.com/m/2W6tUygBb1gAAAAd/gunge-male.gif"
     ],
     "slime": [
-        "https://tenor.com/bESpJ.gif"
+        "https://media1.tenor.com/m/fwUh3G_4g3cAAAAd/slime-sliming.gif"
     ],
     "congrat": [
-        "https://tenor.com/brBMj.gif"
+        "https://media1.tenor.com/m/VOUzW9uvebkAAAAd/dantdm-cbbc.gif"
     ],
     "more": [
-        "https://tenor.com/bXDDI.gif"
+        "https://media1.tenor.com/m/wshzHt1SerQAAAAd/elmo-slime-slime.gif"
     ],
     "yikes": [
-        "https://tenor.com/rciVwrEUYEk.gif"
+        "https://media1.tenor.com/m/xmlrr-H-yGYAAAAd/yikes.gif"
     ],
     "no way": [
-        "https://tenor.com/qOtmsKE03Nv.gif"
+        "https://media1.tenor.com/m/we9F9lBaHKMAAAAd/whaaa-what.gif"
+    ],
+    "love you": [
+        "https://media1.tenor.com/m/GnQIL9rSPWkAAAAC/castanholas.gif"
     ]
 }
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} is online and ready!")
-    await bot.change_presence(activity=discord.Game(name="for: love, hate, felicia"))
 
 @bot.event
 async def on_message(message):
@@ -45,7 +43,13 @@ async def on_message(message):
     msg = message.content.lower()
     for trigger, gifs in meme_database.items():
         if trigger in msg:
-            await message.channel.send(random.choice(gifs))
+            gif_url = random.choice(gifs)
+            
+            # Create embed with GIF
+            embed = discord.Embed(color=0x00ff00)
+            embed.set_image(url=gif_url)
+            
+            await message.channel.send(embed=embed)
             break
 
 @bot.command()
